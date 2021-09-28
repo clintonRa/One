54fb9c8f610f43fce70443490e9f2b3ac3812d80
clintonRa <clintonraj007@gmail.com>
2021-09-28 11:50:19 +0530

    Adding Task

---
+++ src/org/five/Six.java
@@ -0,0 +1,5 @@
+package org.five;
+
+public class Six {
+
+}
---
+++ src/org/two/Three.java
@@ -0,0 +1,13 @@
+package org.two;
+
+public class Three {
+public void four() {
+System.out.println("Hi All");
+}
+public static void main(String[]args) {
+	
+Three t=new Three();
+t.four();
+
+}
+}
